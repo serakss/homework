216f55f5a0c639c8c476b42a007c4633c30cf30b
serakss <serak_96@mail.ru>
2021-12-11 01:11:15 +0300

    aa

---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export function JuniorPlus() {
+    return (
+        <div>
+
+            {/*<HW6/>*/}
+
+        </div>
+    )
+}
+
+
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
