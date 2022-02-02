ee3dbd8455055f9dccc42598eec1d9f0d4b89009
serakss <serak_96@mail.ru>
2022-02-02 23:31:09 +0300

    HW8

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,18 +1,18 @@
 import HW11 from "../../h11/HW11";
 import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
 
 
 export function Junior() {
     return (
         <div>
-         <HW7/>
-
-           {/* <HW11/>*/}
+            <HW7/>
+            <HW8/>
+            {/* <HW11/>*/}
 
         </div>
     )
 }
-
 
 
 // сделайте по аналогии пустые страницы джун и джун+
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,20 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType ={type: 'SORT', payload: 'up'|"down"}|{type: 'CHECK', payload: number}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
+        case 'SORT': {
+          const newState = [...state].sort((a,b)=>{
+              if(a.name > b.name) return 1
+              else if(a.name < b.name) return -1
+              else return  0
+          })
+            return action.payload === "up" ? newState : newState.reverse()
         }
-        case 'check': {
-            // need to fix
-            return state
+        case 'CHECK': {
+
+            return state.filter(f=>f.age >= action.payload)
         }
         default: return state
     }
